4425e902ce4bc9f7f28947ab08d876401ccfcdb9
Mirza Hamzic <mirzaha@maestralsolutions.com>
2022-02-17 14:33:40 +0100

    added user state

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(username, age);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+          name="username"
+        />
+        <label htmlFor="age">Age</label>
+        <input
+          id="age"
+          type="number"
+          onChange={(e) => setAge(e.target.value)}
+          value={age}
+        />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
   );
 };
 
